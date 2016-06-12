d4543621ed6a3c882848ee867b0acbf99aa5be6a
Ronaldo Ortiz <ronyortizop@yahoo.com>
2016-06-11 22:18:09 -0400

    SAT JUN11 10:18pm

--- header.php
+++ header.php
@@ -30,27 +30,33 @@
           <div class="screen-reader-text">
             <?php printf(esc_html__('Go to the home page of %1$s', 'pleiadesweb16')); ?>
           </div>
-          <div class="site-firstletter" aria-hidden="true">
-            <?php echo substr($site_title, 0, 1); ?>
-          </div><!--class="site-firstletter"-->
+          <?php if (has_site_icon()) { ?>
+            <?php $site_icon = esc_url(get_site_icon_url(270));  ?>
+            <img class="site-icon" src="<?php echo $site_icon; ?>" alt="">
+          <?php } else { ?>
+            <div class="site-firstletter" aria-hidden="true">
+              <?php echo substr($site_title, 0, 1); ?>
+            </div><!--class="site-firstletter"-->
+          <?php } ?>
         </a><!--class="site-logo"-->
       </div>
-<!--		BRANDING-->
-		<div class="site-branding<?php if(is_singular()) { echo ' screen-reader-text'; } ?>">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+      <!--		SITE BRANDING-->
+      <div class="site-branding<?php if(is_singular()) { echo ' screen-reader-text'; } ?>">
+        <?php
+        if ( is_front_page() && is_home() ) : ?>
+          <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        <?php else : ?>
+          <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+          <?php
+        endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+        $description = get_bloginfo( 'description', 'display' );
+        if ($description || is_customize_preview()) : ?>
+          <p class="site-description"><?php echo $description; ?></p>
+          <?php
+        endif; ?>
+      </div><!-- .site-branding -->
+
 
 <!--		NAVIGATION-->
     <button id="menu-toggle" class="menu-toggle"><?php _e('MENU', 'pleiadesweb16'); ?></button>
