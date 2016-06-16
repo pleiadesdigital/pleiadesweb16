5bcd6afb4b02ddc885e28a73cc43b7765c99d937
Ronaldo Ortiz <ronyortizop@yahoo.com>
2016-06-15 22:51:13 -0400

    WED JUN15 10:51pm

--- comments.php
+++ comments.php
@@ -24,46 +24,42 @@ if ( post_password_required() ) {
 
 	<?php
 	// You can start editing here -- including this comment!
-	if ( have_comments() ) : ?>
+	if (have_comments()) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'pleiadesweb16' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
+				printf(
+					esc_html(_nx('One comment','%1$s comments', get_comments_number(), 'comments title', 'pleiadesweb16')),
+					number_format_i18n(get_comments_number())
 				);
 			?>
 		</h2>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+		<?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'pleiadesweb16' ); ?></h2>
+			<h2 class="screen-reader-text"><?php esc_html_e('Comment navigation', 'pleiadesweb16'); ?></h2>
 			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'pleiadesweb16' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'pleiadesweb16' ) ); ?></div>
-
+				<div class="nav-previous"><?php previous_comments_link(esc_html__('Older Comments', 'pleiadesweb16')); ?></div>
+				<div class="nav-next"><?php next_comments_link(esc_html__('Newer Comments', 'pleiadesweb16')); ?></div>
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
 		<?php endif; // Check for comment navigation. ?>
 
 		<ol class="comment-list">
 			<?php
-				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				) );
+				wp_list_comments(array(
+					'style'         => 'ol',
+					'short_ping'    => true,
+					'avatar_size'   => '69',
+				));
 			?>
 		</ol><!-- .comment-list -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+		<?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'pleiadesweb16' ); ?></h2>
+			<h2 class="screen-reader-text"><?php esc_html_e('Comment navigation', 'pleiadesweb16'); ?></h2>
 			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'pleiadesweb16' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'pleiadesweb16' ) ); ?></div>
-
+				<div class="nav-previous"><?php previous_comments_link(esc_html__('Older Comments', 'pleiadesweb16')); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__('Newer Comments', 'pleiadesweb16')); ?></div>
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-below -->
 		<?php
@@ -73,8 +69,7 @@ if ( post_password_required() ) {
 
 
 	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-
+	if (!comments_open() && get_comments_number() && post_type_supports(get_post_type(), 'comments')) : ?>
 		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'pleiadesweb16' ); ?></p>
 	<?php
 	endif;
